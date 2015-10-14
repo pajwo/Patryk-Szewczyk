e6d12a349a43d6d4bda6ffa83c196a17d8f9194d
pajwo <sajwo119@gmail.com>
2015-10-14 15:19:54 +0200

    2 laborka

---
+++ laboratorium/02/ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+using namespace std;
+int main()
+{
+	/*
+	double a, b, c, e, f, g, Wx, Wy, W, x, y;
+	cin >> a >> b >> c >> e >> f >> g;
+	W = (a*f) - (b*e);
+	Wx = (c*f) - (f*g);
+	Wy = (a*g) - (c*e);
+	x = (Wx / W);
+	y = (Wy / W);
+	if(W==0)
+	{
+		cout << "???";
+	}
+	cout <<"x= "<< x << endl;
+	cout <<"y= "<<y;
+
+	while (true) {}
+
+	*/
+
+	double a, b, c, d, x1, x2, x3;
+	cin >> a >> b >> c;
+	d = (b*b) - (4 * a*c);
+	x1 = ((-b - sqrt(d))) / (2 * a);
+	x2 = ((-b + sqrt(d))) / (2 * a);
+	x3 = (-b / 2*a);
+
+	if(d>0)
+	{
+		cout << "x1= " << x1;
+		cout << "x2= " << x2;
+	}
+
+	if(d==0)
+	{
+		cout << "x= " << x3;
+	}
+	if (d<0)
+	{
+		cout << "x1= " << x1;
+		cout << "x2= " << x2;
+    }
+
+
+
+	while (true) {}
+
+
+
+	return 0;
+
+}
