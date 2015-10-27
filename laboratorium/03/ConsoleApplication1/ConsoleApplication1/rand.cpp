e0f3057f056eed0d69b34d74f5e33337e1d950b4
pajwo <sajwo119@gmail.com>
2015-10-27 17:26:07 +0100

    wszystko

--- laboratorium/03/ConsoleApplication1/ConsoleApplication1/rand.cpp
+++ laboratorium/03/ConsoleApplication1/ConsoleApplication1/rand.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 int main()
 {
+	srand(time(NULL));
 	const int N = 10;
 	double tab[N];
 	for (int i = 0; i < N; i++)
 	{
 
-		tab[i] = (double)rand() / RAND_MAX;
-		cout << tab[i] << endl;
+		tab[i] = 1.0*rand() / RAND_MAX;
 
 	}
-	cout << endl;
-	for (int i = 0; i < N/2; i++)
-	{
 
-		double dupa = tab[i];
-		tab[i] = tab[N - i - 1];
-		tab[N - i - 1] = dupa;
-			cout << tab[i] << endl;
+	for (int i = 0; i < N - 1; i++)
+		for (int j = 0; j < N - 1; j++)
+			if (tab[j]>tab[j + 1])
+				swap(tab[j], tab[j + 1]);
+
+	for (int i = 0; i < 10; i++)
+		cout << tab[i] << endl;
+
+	cout << endl;
+	for (int i = N - 1; i >= 0; i--)
+	{
+		cout << tab[i] << endl;
 	}
 	system("pause");
 }
