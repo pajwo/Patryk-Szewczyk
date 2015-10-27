fee71c37e00f5bd1962432d86e9c36a5f2be571f
pajwo <sajwo119@gmail.com>
2015-10-27 13:30:52 +0100

    sortowanie

--- 03/03/03.1.cpp
+++ 03/03/03.1.cpp
@@ -8,11 +8,18 @@ int main()
 	{
 
 		tab[i] = (double)rand() / RAND_MAX;
+	}
+
+	for (int i = 0; i < N - 1;i++)
+		for (int j = 0; j < N - 1;j++)
+			if (tab[j]>tab[j + 1])
+				swap(tab[j], tab[j + 1]);
+
+	for (int i = 0; i < 10; i++)
 		cout << tab[i] << endl;
 
-	}
 	cout << endl;
-	for (int i = N-1; i >=0; i--)
+	for (int i = N - 1; i >= 0; i--)
 	{
 		cout << tab[i] << endl;
 	}
