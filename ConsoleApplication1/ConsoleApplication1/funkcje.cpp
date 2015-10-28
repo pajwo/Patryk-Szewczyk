9b2d073c2d5211f5ece0a20b466b56cb28cbe15e
pajwo <sajwo119@gmail.com>
2015-10-28 15:07:37 +0100

    !!!

--- ConsoleApplication1/ConsoleApplication1/funkcje.cpp
+++ ConsoleApplication1/ConsoleApplication1/funkcje.cpp
@@ -4,18 +4,18 @@
 #include <ctime>
 
 using namespace std;
-int n = 200;
+int n = 20;
 vector<char>patryk(int n);
 
 double iloczyn(double x, double y) {
 	return x*y;
 
-	
+
 }
 
 int main()
 {
-    srand(time(NULL));
+	srand(time(NULL));
 
 	cout << iloczyn(8, 9) << endl;
 
@@ -25,11 +25,22 @@ int main()
 	cout << a << ", " << b << endl;
 
 	vector<char>w;
-	for (int i = 0; i < n; i++) {
-		char t = rand() % ('z' - 'a'+1) + 'a';
+	for (int i = 0; i < n; i++) 
+	{	char t = rand() % ('z' - 'a' + 1) + 'a';
 		w.push_back(t);
-		cout << w[i];
 	}
+
+        for (int i = 0; i < n - 1; i++)
+		for (int j = 0; j < n - 1; j++)
+			if (w[j]>w[j + 1])
+				swap(w[j], w[j + 1]);
+
+	    for (int i = 0; i < n; i++)
+		  cout << w[i];
+
+
+
+
 	cout << endl;
 	system("pause");
 }
