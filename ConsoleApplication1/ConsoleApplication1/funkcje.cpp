1b5cb9d95e99cfb70fce038a4efd9bf3455ee3ec
pajwo <sajwo119@gmail.com>
2015-10-28 14:48:22 +0100

    dziala!

--- ConsoleApplication1/ConsoleApplication1/funkcje.cpp
+++ ConsoleApplication1/ConsoleApplication1/funkcje.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <algorithm>
-using namespace std;
+#include <vector>
+#include <ctime>
 
+using namespace std;
+int n = 200;
+vector<char>patryk(int n);
 
 double iloczyn(double x, double y) {
 	return x*y;
+
+	
 }
 
 int main()
 {
+    srand(time(NULL));
+
 	cout << iloczyn(8, 9) << endl;
 
 	int a = 40;
@@ -16,9 +24,12 @@ int main()
 	swap(a, b);
 	cout << a << ", " << b << endl;
 
-	
-
-
-  system("pause");
-
+	vector<char>w;
+	for (int i = 0; i < n; i++) {
+		char t = rand() % ('z' - 'a'+1) + 'a';
+		w.push_back(t);
+		cout << w[i];
+	}
+	cout << endl;
+	system("pause");
 }
