55897891f099ff361a91a4a96aadc1727d4a9b7b
pajwo <sajwo119@gmail.com>
2015-10-28 14:24:52 +0100

    swap

---
+++ ConsoleApplication1/ConsoleApplication1/funkcje.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+double iloczyn(double x, double y) {
+	return x*y;
+}
+
+int main()
+{
+	cout << iloczyn(8, 9) << endl;
+
+	int a = 40;
+	int b = 60;
+	swap(a, b);
+	cout << a << ", " << b << endl;
+
+	
+
+
+  system("pause");
+
+}
