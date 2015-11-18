d9a2d93bc9bdf38b623abfe28973b62c6df56f6f
pajwo <sajwo119@gmail.com>
2015-11-18 15:10:16 +0100

    1.2

--- wskazniki/wskazniki/wskazniki.cpp
+++ wskazniki/wskazniki/wskazniki.cpp
@@ -17,7 +17,23 @@ void wyswietl(double *tab, int N, ostream& plik)
 	for (int i = 0; i < N; i++)
 		plik << *(tab + i) << endl;
 }
+// Cwiczenie nr 2 - ca³ka
+double calka(double a, double b, double dx, double(*pf)(double))
+{
+	double Pole = 0
+		for (double i = a; i <= b; i += dx)
+		{
 
+		}
+}
+
+//Cwiczenie nr 3
+
+double kwadrat(double x) { return x*x; }
+double sinus(double x) { return sin(x);}
+double fikusna(double x) { return x*x + 2773 / x;}
+
+//Koniec cwiczenia nr 3
 
 int main()
 {
@@ -30,3 +46,4 @@ int main()
 
 		system("pause");
 }
+
