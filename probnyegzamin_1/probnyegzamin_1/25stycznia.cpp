9c6bf6e96803b9f08b921bef481f353ba02afc3d
pajwo <sajwo119@gmail.com>
2016-01-25 14:36:44 +0100

    cdc

--- probnyegzamin_1/probnyegzamin_1/25stycznia.cpp
+++ probnyegzamin_1/probnyegzamin_1/25stycznia.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct lekarz
+{
+	string imie, nazwisko;
+	lekarz * lewy, prawy;
+	wizyta * wizyty;
+
+};
+struct wizyta {
+	string imie, nazwisko;
+	string data_wizyty;
+	wizyta * next,
+		*prev;
+};
 
 int main()
 {
